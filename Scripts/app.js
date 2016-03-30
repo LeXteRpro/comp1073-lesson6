9a13f8cd07abd34c369b576d5184cf378ef02a0f
LeXteRpro <lexter@hotmail.ca>
2016-03-30 16:13:08 -0400

    Pushing Arrays

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,18 @@
 
     "use strict";
 
+var myVariable = "hello!";
+
+console.log("myVariable " + myVariable);
+
+
+var myArray = [];
+
+myArray.push("My First Greeting!");
+
+myArray.push("My Second Greeting!");
+
+console.log(myArray);
 
 })();
 
