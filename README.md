b7661d0ad162394533486a29bd42a8678785e10f
LeXteRpro <lexter@hotmail.ca>
2016-03-30 15:52:12 -0400

    Taking Github Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# COMP1073-Lesson6
+# COMP1073-Lesson8
+
+COMP1073 Lesson 8 for Client-Scripting @ Georgian College
--- Scripts/app.js
+++ Scripts/app.js
@@ -1,89 +1,54 @@
-"use strict";
-
 // setup your IIFE (Immediately Invoked Function Expression)
+(function() {
 
-(function () {
+    "use strict";
+    var output = "";
+    var firstParagraph = document.getElementById("firstParagraph");
 
+    // Instantiate new xhr object
+    var request = new XMLHttpRequest();
+    request.open('GET', '../person.txt', true);
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4) {
+            var Person = {}; /* var Person = new Object();   */
 
-    if(document.getElementById("home") != null) {
-        console.log("Home Page");
+            Person = JSON.parse(request.responseText);
+            
+            Person.sayHello = function() {
+                output += "<br><hr><br>" + Person.name + " says hello";
+            }
 
-    } else if (document.getElementById("projects") != null) {
-        console.log("Projects Page");
+            // for every key in the Person object, loop...
+            for (var key in Person) {
 
+                // check if the key is the familyNames array
+                if (key === "familyNames") {
+                    output += "<br>Family Names: <br>";
+                    output += "<hr><br>";
+                    output += "<ul>";
+                    for (var index = 0; index < Person.familyNames.length; index++) {
+                        output += "<li>" + Person.familyNames[index] + "</li>";
+                    } // for loop
+                    output += "</ul>";
+                } // if statement
+                else if (key === "sayHello") {
+                    Person.sayHello();
+                }
+                
+                // for all other cases do the following...
+                else {
 
-    } else if (document.getElementById("contact") != null) {
+                    output += Person[key] + "<br>";
+                } // else statement
 
-        console.log("Contact Page");
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-        var checkMeOut = document.getElementById("checkMeOut");
-        var submitButton = document.getElementById("submitButton");
+            } // for in
 
-        // Checkbox Event Listener 
-        checkMeOut.addEventListener("change", function () {
-
-            if (checkMeOut.checked) {
-                console.log("It's checked!");
-            } else {
-                 console.log("It's Unchecked!");
-            } // end else
-         }); // end addEventListener
-
-        submitButton.addEventListener("click", function(event) {
-                    event.preventDefault();
-                    console.log("Email: " + email.value);
-                    console.log("Password: " + password.value);
-                });
-
-        } // End else if
-
- })();
-
-
-
-
-
-    
-  // checkMeOut.onclick(function() {
-    //     console.log("checkMeOut clicked");
-    // });
-
-
-      // (checkMeOut.checked) ? console.log("It's checked") ; console.log("It's checked");
-
-
-   
-     // a little nicer but counting down
-    /*
-    for(var index= paragraphData.length-1; index > -1; index--) {
-        console.log(paragraphData[index]);
-    }
-    */
-    
-    
-    /* even nicer but more complicated
-    paragraphData.forEach(function(paragraph) {
-        console.log(paragraph);
+            firstParagraph.innerHTML = output;
+        }
     });
-    */
-  
-    //declared a named function way
 
-//     function replaceFirstParagraph() {
-//         console.log("inside replaceFirstParagraph function");
-//         var firstParagraph;
-
-//         firstParagraph = document.getElementById("firstParagraph");
-
-//         firstParagraph.innerHTML = "My New Paragraph Data";
-        
-//     }
-
-   
-//     // call the replaceFirstParagraph function
-//     replaceFirstParagraph();
+    request.send();
 
 
-// })();
+})();
 
