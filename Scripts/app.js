1841859274114fcc9687ee30ca4cbecacd603319
LeXteRpro <lexter@hotmail.ca>
2016-03-30 21:08:28 -0400

    Fixed XML Request

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,65 +2,56 @@
 (function() {
 
     "use strict";
-
     var output = "";
     var firstParagraph = document.getElementById("firstParagraph");
 
-    var person = {
 
-        "age": 40,
-        "name": "Orie",
-        "address": "1 Georgian Drive",
-        "city": "Barrie",
-        "province":"Ontario",
+    // Instantiate new xhr
+    var request = new XMLHttpRequest();
+    request.open('GET', '../person.txt', true);
+    request.addEventListener('readystatechange', function() {
 
-        "familyNames": [
-        "Harrison",
-        "Marissa",
-        "Dino",
-        "Alexander"
-        ],
+        if (request.readyState === 4) {
 
-        "sayHello": function(){
-            output += "<br><hr><br>"
-            output += person.name + " says hello";
+           var person = {}; /* Var person = new Object(); */
+
+           person = JSON.parse(request.responseText);
+
+           console.log(person);
+
+
+    // for every key in the Person Object, loop...
+    for(var key in person) {
+
+        if (key == "familyNames") {
+
+            output += "<br>Family Names: <br>";
+
+            output += "<hr><br>";
+
+            output += "<ul>";
+
+            for(var index = 0; index < person.familyNames.length; index++) {
+                output += "<li>" + person.familyNames[index];
+            } // close for loop
+            output += "</ul>";
+        } // close if statement
+
+
+        // Check if the key is the sayHello method
+        else if (key == "sayHello") {
+            person.sayHello();
         }
-        
-    }; /* Var person = new Object(); */
 
-
-
-// for every key in the Person Object, loop...
-for(var key in person) {
-
-    if (key == "familyNames") {
-
-        output += "<br>Family Names: <br>";
-
-        output += "<hr><br>";
-
-        output += "<ul>";
-
-        for(var index = 0; index < person.familyNames.length; index++) {
-            output += "<li>" + person.familyNames[index];
-        } // close for loop
-        output += "</ul>";
-    } // close if statement
-
-
-    // Check if the key is the sayHello method
-    else if (key == "sayHello") {
-        person.sayHello();
+        // For all other cases, do the following...
+        else {
+            output += person[key] + "<br>";
+        } // else statement
     }
 
-    // For all other cases, do the following...
-    else {
-        output += person[key] + "<br>";
-    } // else statement
 }
 
-
-firstParagraph.innerHTML = output;
+});
 
 })();
 
